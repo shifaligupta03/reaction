f1d7477a9ff575cd47e01c3e5af82793d58a4271
shifali <shifali.gupta183@gmail.com>
2019-07-01 00:03:33 +0530

    reaction component

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAppContext } from './hooks';
+import CreateReaction from './CreateReaction';
 
 const MessageBoard = ()=>{
     const {state: {messages}} = useAppContext();
@@ -12,6 +13,7 @@ const MessageBoard = ()=>{
                         <h4>{new Date(timestamp).toLocaleString()}</h4>
                         <p>{text}</p>
                         <h4>- {username}</h4>
+                        <CreateReaction />
                         <hr />
                     </div>
 
